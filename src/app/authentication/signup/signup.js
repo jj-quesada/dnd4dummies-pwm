f865a820c3132c14111da3ad260266586bb4ec46
Álvaro <alvarosacosta22@gmail.com>
2024-03-11 19:59:19 +0000

    Avances en formulario de validacion de registro

--- src/app/authentication/signup/signup.js
+++ src/app/authentication/signup/signup.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function() {
     await cargarEstructura();
+    await confirmInputs();
 });
 
 async function cargarEstructura() {
@@ -38,4 +39,59 @@ function interactLoginPopup() {
     } else {
         document.querySelector('.login-popup-container').style.display = 'none';
     }
+}
+
+async function confirmInputs() {
+    const form = document.querySelector('.user-info');
+
+    const password = document.querySelector('.parameter__text--password');
+    const confirmPassword = document.querySelector('.parameter__text--confirm-password');
+
+    const email = document.querySelector('.parameter__text--email');
+    const confirmEmail = document.querySelector('.parameter__text--confirm-email');
+
+    const birthdate = document.querySelector('.parameter__text--birth-date');
+
+    const warnings = document.querySelector('.warnings');
+
+    form.addEventListener('submit', e=>{
+        e.preventDefault();
+
+        let helper = false; 
+        let actualWarnings = "";
+        let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+        let actualTime = new Date();
+        let minimumYear = parseInt(actualTime.getFullYear() - 18);
+
+        let userTime = new Date(birthdate.value);
+        let userYear = parseInt(userTime.getFullYear());
+        
+        if(!regexEmail.test(email.value)){
+            actualWarnings += `That email is not valid. <br>`;
+            helper = true;
+        }
+        
+        if (email.value !== confirmEmail.value) {
+            actualWarnings += `The emails are not the same. <br>`;
+            helper = true;
+        } 
+
+        if (password.value !== confirmPassword.value) {
+            actualWarnings += `The passwords are not the same. <br>`;
+            helper = true;
+        }
+
+        if(userYear > minimumYear){
+            actualWarnings += `You are underage. GET OUT! <br>`;
+            helper = true;
+        }
+
+        if(helper){
+            warnings.innerHTML = actualWarnings;
+        } else {
+            warnings.innerHTML = "Done!";
+        }
+
+    });
 }
