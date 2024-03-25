128810c8aad1eb7ad8e94b6580c31e912d0a411b
Juan José Quesada Acosta <jj.quesadaacosta@hotmail.com>
2024-03-25 19:36:35 +0000

    Generalizado a un JS de utilidades las funciones para cargar elementos al glosario desde un JSON y añadido JSON de Spells

--- src/app/glossary/interaction-related/spells/spells.js
+++ src/app/glossary/interaction-related/spells/spells.js
@@ -1,36 +1,9 @@
-import { cargarEstructura, cargarTemplate } from '/src/app/fetch.js';
+import { cargarEstructura } from '/src/app/fetch.js';
+import { loadElementListFromJSON, cargarSectionCard } from '/src/app/glossary/glossary-utilities.js';
 
 document.addEventListener('DOMContentLoaded', async function() {
     await cargarEstructura('/src/app/glossary/components/descriptive-glossary/descriptive-glossary.html');
     document.querySelector('.banner__hover-text').textContent = "Spells";
-    await loadElementList();
+    await loadElementListFromJSON('spells.json', 'allSpells');
+    await cargarSectionCard();
 });
-
-async function loadElementList() {
-    let elementList = document.querySelector('.elements-section__list');
-
-    const templateURL = '/src/app/glossary/components/descriptive-glossary-element/descriptive-glossary-element.html';
-
-    // Inicializar un array para almacenar las promesas
-    let promises = [];
-
-    // Crear 4 elementos li, asignarles la clase 'element' y cargar el template
-    for (let i = 0; i < 4; i++) {
-        let li = document.createElement('li');
-        li.classList.add('element');
-
-        // Añadir el li al ul
-        elementList.appendChild(li);
-
-        // Cargar el template y añadirlo al li
-        let promise = cargarTemplate(templateURL).then(content => {
-            li.appendChild(content);
-        });
-
-        // Añadir la promesa al array de promesas
-        promises.push(promise);
-    }
-
-    // Esperar a que todas las promesas se resuelvan
-    await Promise.all(promises);
-}
