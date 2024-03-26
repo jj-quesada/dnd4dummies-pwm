5954c302077dc31d8c00bcfbf25df490371c820d
Álvaro <alvarosacosta22@gmail.com>
2024-03-26 11:09:43 +0000

    side-bar terminado

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function() {
     await cargarEstructura();
+    await sideBar();
 });
 
 async function cargarEstructura() {
@@ -27,6 +28,10 @@ async function cargarLoginPopup() {
     document.querySelector('.upper-bar__login-logo-image').addEventListener('click', function() {
         interactLoginPopup();
     });
+    document.querySelector('.side-bar__login-logo-image').addEventListener('click', function() {
+        interactLoginPopup();
+        document.querySelector('.side-bar').classList.remove("visible");
+    });
     document.querySelector('.login-container__close-icon').addEventListener('click', function() {
         interactLoginPopup();
     });
@@ -38,4 +43,24 @@ function interactLoginPopup() {
     } else {
         document.querySelector('.login-popup-container').style.display = 'none';
     }
+}
+
+async function sideBar() {
+    const nav = document.querySelector('.side-bar');
+
+    document.querySelector('.abrir').addEventListener("click", () => {
+        nav.classList.add("visible");
+        document.querySelector('.login-popup-container').style.display = 'none';
+    })
+
+    document.querySelector('.cerrar').addEventListener("click", () => {
+        nav.classList.remove("visible");
+    })
+
+    document.addEventListener("mousedown", (event) => {
+        const target = event.target;
+        if (!nav.contains(target) && target !== document.querySelector('.abrir')) {
+            nav.classList.remove("visible");
+        }
+    });
 }
--- src/app/shared/header/header.js
+++ src/app/shared/header/header.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function() {
     await cargarLoginPopup();
+    await sideBar();
 
 });
 
@@ -19,6 +20,10 @@ async function cargarLoginPopup() {
     document.querySelector('.upper-bar__login-logo-image').addEventListener('click', function() {
         interactLoginPopup();
     });
+    document.querySelector('.side-bar__login-logo-image').addEventListener('click', function() {
+        interactLoginPopup();
+        document.querySelector('.side-bar').classList.remove("visible");
+    });
     document.querySelector('.login-container__close-icon').addEventListener('click', function() {
         interactLoginPopup();
     });
@@ -30,4 +35,24 @@ function interactLoginPopup() {
     } else {
         document.querySelector('.login-popup-container').style.display = 'none';
     }
+}
+
+async function sideBar() {
+    const nav = document.querySelector('.side-bar');
+
+    document.querySelector('.abrir').addEventListener("click", () => {
+        nav.classList.add("visible");
+        document.querySelector('.login-popup-container').style.display = 'none';
+    })
+
+    document.querySelector('.cerrar').addEventListener("click", () => {
+        nav.classList.remove("visible");
+    })
+
+    document.addEventListener("mousedown", (event) => {
+        const target = event.target;
+        if (!nav.contains(target) && target !== document.querySelector('.abrir')) {
+            nav.classList.remove("visible");
+        }
+    });
 }
